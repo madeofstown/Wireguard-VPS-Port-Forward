b7db5a570c3a10f16137d3fc3604ed40f89e2669
madeofstown <33820964+madeofstown@users.noreply.github.com>
2024-02-03 16:56:31 -0800

    Update PostDown.example.sh
    Finally taking another crack at it

--- VPS/PostDown.example.sh
+++ VPS/PostDown.example.sh
@@ -10,9 +10,24 @@ TCP_PORTS="25 80 443 2222"
 
 for p in $TCP_PORTS
 do
-    iptables -t nat -D PREROUTING -i $ni -p tcp --dport $p -j DNAT --to-destination 10.7.0.2:$p
+    iptables -t nat -D PREROUTING -i $ni -p tcp --dport $p -j DNAT --to-destination 10.27.27.2:$p
     ip6tables -t nat -D PREROUTING -i $ni -p tcp --dport $p -j DNAT --to-destination fd07:dead:feed::2:$p  
 done
 
-iptables -t nat -D POSTROUTING -o $ni -j SNAT --to-source <1.2.3.4 - VPS Public IPv4>
-ip6tables -t nat -D POSTROUTING -o $ni -j SNAT --to-source <2700::1234 - VPS Public IPv6>
+UDP_PORTS="19132"
+
+for p in $UDP_PORTS
+do
+    iptables -t nat -D PREROUTING -i $ni -p udp --dport $p -j DNAT --to-destination 10.27.27.2:$p
+    ip6tables -t nat -D PREROUTING -i $ni -p udp --dport $p -j DNAT --to-destination fd07:dead:feed::2:$p  
+done
+
+ICMP_FORWARD="false"
+
+if [ $ICMP_FORWARD = "true" ]; then
+    iptables -t nat -D PREROUTING -i $ni -p icmp -j DNAT --to-destination 10.27.27.2:$p
+    ip6tables -t nat -D PREROUTING -i $ni -p icmp -j DNAT --to-destination fd07:dead:feed::2:$p 
+fi
+
+iptables -t nat -D POSTROUTING -o $ni -j MASQUERADE
+ip6tables -t nat -D POSTROUTING -o $ni -j MASQUERADE
