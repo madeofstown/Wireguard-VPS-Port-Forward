55d181407cca168b07479b52b95ba9f9e624a06e
madeofstown <33820964+madeofstown@users.noreply.github.com>
2024-02-03 17:54:15 -0800

    Update PostDown.example.sh

--- VPS/PostDown.example.sh
+++ VPS/PostDown.example.sh
@@ -11,7 +11,7 @@ TCP_PORTS="25 80 443 2222"
 for p in $TCP_PORTS
 do
     iptables -t nat -D PREROUTING -i $ni -p tcp --dport $p -j DNAT --to-destination 10.27.27.2:$p
-    ip6tables -t nat -D PREROUTING -i $ni -p tcp --dport $p -j DNAT --to-destination fd07:dead:feed::2:$p  
+    ip6tables -t nat -D PREROUTING -i $ni -p tcp --dport $p -j DNAT --to-destination fd07:dead:feed::2:$p
 done
 
 UDP_PORTS="19132"
@@ -19,14 +19,14 @@ UDP_PORTS="19132"
 for p in $UDP_PORTS
 do
     iptables -t nat -D PREROUTING -i $ni -p udp --dport $p -j DNAT --to-destination 10.27.27.2:$p
-    ip6tables -t nat -D PREROUTING -i $ni -p udp --dport $p -j DNAT --to-destination fd07:dead:feed::2:$p  
+    ip6tables -t nat -D PREROUTING -i $ni -p udp --dport $p -j DNAT --to-destination fd07:dead:feed::2:$p
 done
 
 ICMP_FORWARD="false"
 
 if [ $ICMP_FORWARD = "true" ]; then
     iptables -t nat -D PREROUTING -i $ni -p icmp -j DNAT --to-destination 10.27.27.2:$p
-    ip6tables -t nat -D PREROUTING -i $ni -p icmp -j DNAT --to-destination fd07:dead:feed::2:$p 
+    ip6tables -t nat -D PREROUTING -i $ni -p icmp -j DNAT --to-destination fd07:dead:feed::2:$p
 fi
 
 iptables -t nat -D POSTROUTING -o $ni -j MASQUERADE
